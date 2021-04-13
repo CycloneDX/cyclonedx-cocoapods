57c97705c2c8f30139b5bcb9393ccad47704e3ac
José González <jose.gonzalez.jg24@businesspartner.roche.com>
2021-04-13 16:47:21 +0200

    Added Component class to model component being described by the BOM

---
+++ spec/cyclonedx/cocoapods/component_spec.rb
@@ -0,0 +1,63 @@
+require 'rspec'
+require 'cyclonedx/cocoapods/component'
+
+RSpec.describe CycloneDX::CocoaPods::Component do
+  context 'when creating new components' do
+    let(:group) { 'application-group' }
+    let(:name) { 'Application' }
+    let(:version) { '1.3.5' }
+    let(:type) { described_class::VALID_COMPONENT_TYPES.sample }
+
+    context 'with an empty group' do
+      it 'should raise an error' do
+        expect { described_class.new(group: '    ', name: name, version: version, type: type) }.to raise_error(ArgumentError, "Group, if specified, must be non empty")
+      end
+    end
+
+    context 'with a nil name' do
+      it 'should raise an error' do
+        expect { described_class.new(name: nil, version: version, type: type) }.to raise_error(ArgumentError, "Name must be non empty")
+      end
+    end
+
+    context 'with an empty name' do
+      it 'should raise an error' do
+        expect { described_class.new(name: '   ', version: version, type: type) }.to raise_error(ArgumentError, "Name must be non empty")
+      end
+    end
+
+    context 'with an invalid version' do
+      it 'should raise an error' do
+        expect { described_class.new(name: name, version: 'not-a-valid.version', type: type) }.to raise_error(ArgumentError, /Malformed version number/)
+      end
+    end
+
+    context 'with an invalid type' do
+      it 'should raise an error' do
+        expect { described_class.new(name: name, version: version, type: 'invalid-type') }.to raise_error(ArgumentError, /is not valid component type/)
+      end
+    end
+
+    context 'with valid values' do
+      context 'without group' do
+        it 'should properly build the component' do
+          component = described_class.new(name: name, version: version, type: type)
+          expect(component.group).to be_nil
+          expect(component.name).to eq(name)
+          expect(component.version).to eq(version)
+          expect(component.type).to eq(type)
+        end
+      end
+
+      context 'with group' do
+        it 'should properly build the component' do
+          component = described_class.new(group: group, name: name, version: version, type: type)
+          expect(component.group).to eq(group)
+          expect(component.name).to eq(name)
+          expect(component.version).to eq(version)
+          expect(component.type).to eq(type)
+        end
+      end
+    end
+  end
+end
