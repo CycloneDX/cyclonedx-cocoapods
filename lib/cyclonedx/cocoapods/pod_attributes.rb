6b83e767fce75eeb471d6e0d4482ce91a18425ca
José González <jose.gonzalez.jg24@businesspartner.roche.com>
2021-05-21 13:52:01 +0200

    Moved search code to pod sources

---
+++ lib/cyclonedx/cocoapods/pod_attributes.rb
@@ -0,0 +1,46 @@
+module CycloneDX
+  module CocoaPods
+    class SearchError < StandardError; end
+
+    module Source
+      class CocoaPodsRepository
+        attr_accessor :source_manager
+
+        def self.searchable_source(url:, source_manager:)
+          source = CocoaPodsRepository.new(url: url)
+          source.source_manager = source_manager
+          return source
+        end
+
+        def attributes_for(pod:)
+          specification_sets = @source_manager.search_by_name("^#{Regexp.escape(pod.root_name)}$")
+          raise SearchError, "No pod found named #{pod.name}" if specification_sets.length == 0
+          raise SearchError, "More than one pod found named #{pod.name}" if specification_sets.length > 1
+
+          paths = specification_sets[0].specification_paths_for_version(pod.version)
+          raise SearchError, "Version #{pod.version} not found for pod #{pod.name}" if paths.length == 0
+
+          ::Pod::Specification.from_file(paths[0]).attributes_hash
+        end
+      end
+
+      class GitRepository
+        def attributes_for(pod:)
+          {} # TODO: Retrieve attributes from podspec in git repository
+        end
+      end
+
+      class LocalPod
+        def attributes_for(pod:)
+          {} # TODO: Retrieve attributes from podspec in local file system
+        end
+      end
+
+      class Podspec
+        def attributes_for(pod:)
+          {} # TODO: Retrieve attributes from podspec in specified location
+        end
+      end
+    end
+  end
+end
---
+++ spec/cyclonedx/cocoapods/pod_attributes_spec.rb
@@ -0,0 +1,85 @@
+require 'rspec'
+require 'cyclonedx/cocoapods/pod_attributes'
+require 'cyclonedx/cocoapods/pod'
+
+# This is far from a black box type of unit testing, but it serves the purpose of documenting
+# our use of the CocoaPods classes and the expected return values, which may change between versions
+# of CocoaPods.
+RSpec.describe CycloneDX::CocoaPods::Source::CocoaPodsRepository do
+  let(:pod) { CycloneDX::CocoaPods::Pod.new(name: 'Alamofire/Core', version: '5.4.2') }
+  let(:paths) { ['/cocoapods/repos/repo1/pod/pod.json', '/cocoapods/repos/repo2/pod/pod.json'] }
+  let(:attributes) { { name: pod.name, version: pod.version } }
+
+  before(:each) do
+    @source_manager = double()
+    @specification_set = double()
+    @specification = double()
+
+    allow(@source_manager).to receive(:search_by_name).and_return([@specification_set])
+    allow(@specification_set).to receive(:specification_paths_for_version).and_return(paths)
+    allow(::Pod::Specification).to receive(:from_file).and_return(@specification)
+    allow(@specification).to receive(:attributes_hash).and_return(attributes)
+
+    @source = described_class.searchable_source(url: described_class::CDN_REPOSITORY, source_manager: @source_manager)
+  end
+
+  context 'searching for a pod' do
+    it 'should search for a pod with that exact root name and version' do
+      expect(@source_manager).to receive(:search_by_name).with("^#{pod.root_name}$")
+      expect(@specification_set).to receive(:specification_paths_for_version).with(pod.version)
+      @source.attributes_for(pod: pod)
+    end
+
+    it 'should properly escape the name of the pod' do
+      pod_with_special_name = CycloneDX::CocoaPods::Pod.new(name: 'R.swift', version: '1.0.0')
+      expect(@source_manager).to receive(:search_by_name).with("^#{Regexp.escape(pod_with_special_name.name)}$")
+      expect(@specification_set).to receive(:specification_paths_for_version).with(pod_with_special_name.version)
+      @source.attributes_for(pod: pod_with_special_name)
+    end
+
+    context 'when the source manager doesn''t find any pod with the provided name' do
+      before(:each) do
+        allow(@source_manager).to receive(:search_by_name).and_return([])
+      end
+
+      it 'should raise an error' do
+        expect {
+          @source.attributes_for(pod: pod)
+        }.to raise_error(CycloneDX::CocoaPods::SearchError, "No pod found named #{pod.name}")
+      end
+    end
+
+    context 'when the source manager finds more than one pod with the provided name' do
+      before(:each) do
+        allow(@source_manager).to receive(:search_by_name).and_return([@specification_set, @specification_set])
+      end
+
+      it 'should raise an error' do
+        expect {
+          @source.attributes_for(pod: pod)
+        }.to raise_error(CycloneDX::CocoaPods::SearchError, "More than one pod found named #{pod.name}")
+      end
+    end
+
+    context 'when the source manager finds exactly one pod with the provided name' do
+      context 'when the search manager doesn''t find an specification for the provided version' do
+        before(:each) do
+          allow(@specification_set).to receive(:specification_paths_for_version).and_return([])
+        end
+
+        it 'should raise an error' do
+          expect {
+            @source.attributes_for(pod: pod)
+          }.to raise_error(CycloneDX::CocoaPods::SearchError, "Version #{pod.version} not found for pod #{pod.name}")
+        end
+      end
+
+      context 'when the source manager finds at least a specification for the provided version' do
+        it 'returns the attributes of the first specification' do
+          expect(::Pod::Specification).to receive(:from_file).with(paths[0])
+          expect(@source.attributes_for(pod: pod)).to eq(attributes)
+        end
+      end
+    end
+  end
+end
