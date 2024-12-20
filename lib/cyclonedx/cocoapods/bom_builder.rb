2936890995a6e6f319ee2b1c6894954a1ecfa147
Marlon Pina Tojal <marlont@backbase.com>
2024-12-20 09:09:24 +0100

    add comment and test
    Signed-off-by: Marlon Pina Tojal <marlont@backbase.com>

--- lib/cyclonedx/cocoapods/bom_builder.rb
+++ lib/cyclonedx/cocoapods/bom_builder.rb
@@ -127,6 +127,7 @@ module CycloneDX
           xml_add_author(xml, trim_strings_length)
           xml.name_ name
           xml.version version.to_s
+          # Use `dump` to escape non-printing characters, then remove the starting and trailing double-quotes from `dump`.
           xml.description { xml.cdata description.dump[1..-2] } unless description.nil?
           unless checksum.nil?
             xml.hashes do
